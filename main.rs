6a08883b1ec3d52dcc87f6ece3ed43272b0150af
dzigibro <i1w1zwsjp@mozmail.com>
2025-06-17 17:19:06 +0200

    refactor

--- main.rs
+++ main.rs
@@ -1,24 +1,11 @@
 use std::io::{BufRead, BufReader};
 use std::process::{Child, Command, Stdio};
 use rand::Rng;
+mod spawn_journal;
 
 fn main() {
-    // Start journalctl as a child process
-    let mut child = Command::new("journalctl")
-        .arg("-f")
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to spawn journalctl");
-
-    let stdout = child.stdout.take().unwrap();
-    let reader = BufReader::new(stdout);
-
-    // Stream each line, checking for matches
-    for line_result in reader.lines() {
-        if let Ok(line) = line_result {
-            check_line_for_matches(&line);
-        }
-    }
+    spawn_journal::spawnjournal();
+    
 }
 
 fn check_line_for_matches(line: &str) {
