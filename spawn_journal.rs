40013f7b609571b99c05fc0fb29afcfc800b66dc
dzigibro <dzigibro@protonmail.com>
2025-06-19 12:33:25 +0200

    Removing fucntion dependency, refactoring

--- spawn_journal.rs
+++ spawn_journal.rs
@@ -1,9 +1,38 @@
 use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader};
 
+use crate::check_line_for_matches;
 
 
-pub fn spawnjournal(){// Start journalctl as a child process
+// Start journalctl as a child process
+
+fn spawnjournal2<F: Fn(&str)>(callback: F){
+
+let mut child = Command::new("journalctl")
+
+.arg("-f")
+
+.stdout(Stdio::piped())
+
+.spawn()
+
+.expect("Cant Spawn journal bro check commands?");
+
+let stdoout = child.stdout.take().unwrap();
+let reader = BufReader::new(stdoout);
+
+for line_result in reader.lines(){
+    if let Ok(line)=line_result
+{
+    callback(&line);
+
+
+}
+}
+
+
+}
+pub fn spawnjournal(){
     let mut child = Command::new("journalctl")
         .arg("-f")
         .stdout(Stdio::piped())
