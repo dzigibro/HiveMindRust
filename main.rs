b08468c960f5bac0f08c13ac6cdc853a3b6ed32f
dzigibro <dzigibro@protonmail.com>
2025-06-19 12:33:54 +0200

    Native journal access, faster regex

--- main.rs
+++ main.rs
@@ -6,7 +6,7 @@ mod spawn_journal;
 use regex::Regex;
 use systemd::journal::{Journal, JournalSeek, OpenOptions};
 
-//REGEX setup once a runtime = fast! Nice! Efficent! -----Should add the fucking outside import to handle these in yaml instead 
+//REGEX setup once a runtime = fast! Nice! Efficent! -----Should add the fucking outside import to handle these in yaml or json instead 
 lazy_static::lazy_static! {
     static ref PATTERNS: Vec<Regex> = vec![
         Regex::new(r"failed").unwrap(),
